3233db979963ab5e6fc827b3b049af00cbe8aad5
serkanboyraci <65953412+serkanboyraci@users.noreply.github.com>
2022-12-25 14:10:48 +0300

    comments added

--- SendingDataWithNotificationCenter/SendingDataWithNotificationCenter/FirstViewController.swift
+++ SendingDataWithNotificationCenter/SendingDataWithNotificationCenter/FirstViewController.swift
@@ -9,23 +9,23 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
-    @IBOutlet weak var receivedTextLabel: UILabel!
+    @IBOutlet weak var receivedTextLabel: UILabel! // we define label
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         NotificationCenter.default.addObserver(self, selector: #selector(getData(data: )), name: NSNotification.Name(rawValue: "getDataID"), object: nil)
-    
+    // to take notification from the NotificationCenter. to take it correctly "rawValue" must be the same with post notification.
     }
-    @objc func getData(data:Notification) {
-        if let userInfo = data.userInfo{
-            let receivedData = userInfo["ReceivedData"] as! String
-            receivedTextLabel.text = "Received Text: \(receivedData)"
+    @objc func getData(data:Notification) { // selector function
+        if let userInfo = data.userInfo{ // to take data by selector function.
+            let receivedData = userInfo["ReceivedData"] as! String 
+            receivedTextLabel.text = "Received Text: \(receivedData)" // to show data, we define in label.
         }
     }
     
-    @IBAction func getDataClicked(_ sender: Any) {
-        performSegue(withIdentifier: "toSendData", sender: nil)
+    @IBAction func getDataClicked(_ sender: Any) { // we define Get Data Button
+        performSegue(withIdentifier: "toSendData", sender: nil)  //to go secondVC, we use segue
     }
 }
 
