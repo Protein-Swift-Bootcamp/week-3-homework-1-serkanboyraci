e762b075369538ed677349e7b5e23c414f2dbba4
serkanboyraci <65953412+serkanboyraci@users.noreply.github.com>
2022-12-25 14:14:33 +0300

    comments added

--- SendingDataWithNotificationCenter/SendingDataWithNotificationCenter/SecondViewController.swift
+++ SendingDataWithNotificationCenter/SendingDataWithNotificationCenter/SecondViewController.swift
@@ -9,14 +9,16 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
-    @IBOutlet weak var dataTextField: UITextField!
+    @IBOutlet weak var dataTextField: UITextField! // defined TextField
     
    override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-    @IBAction func sendDataClicked(_ sender: Any) {
+    @IBAction func sendDataClicked(_ sender: Any) { //defined sendData Button.
         NotificationCenter.default.post(name: .init(rawValue: "getDataID"), object: nil,  userInfo:["ReceivedData":dataTextField.text!])
+        // to send data, we post Notification with NotificationCenter and we put data in userInfo dictionary and give id as a "getDataID"
+        // rawValue must be same for taking data from NotificationCenter.addObserver 
         
         self.dismiss(animated: true, completion: nil) // to close second VC
 
